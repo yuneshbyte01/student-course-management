b2c27b400119f4bdd8f7a8d6b3912de3d1324ee7
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-06-12 16:40:31 +0545

    first commit

---
+++ src/test/java/com/yuneshtimsina/studentcoursemanagement/StudentCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuneshtimsina.studentcoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
